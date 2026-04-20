645056b41209812abc7b30f8d8a64a8a83f50fb1
toshan kanwar <toshankanwar03@gmail.com>
2026-04-20 11:58:45 +0530

    about page

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "static-assets.toshankanwar.in",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
